25d3176dc1d8d56701f32f3a45e6427ea0d9728d
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2019-03-11 21:45:52 -0300

    Improve the auction component type 3
    -Added a '+' option to show the charts

--- src/app/auctions/auction/auction.module.ts
+++ src/app/auctions/auction/auction.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ThfModule } from '@totvs/thf-ui';
+import { ThfKendoModule } from '@totvs/thf-kendo';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuctionComponent } from './auction.component';
+
 
 @NgModule({
     declarations:[
@@ -10,7 +14,10 @@ import { AuctionComponent } from './auction.component';
     ],
     imports: [
         CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
         ThfModule,
+        ThfKendoModule,
         SharedModule
     ],
     exports: [
